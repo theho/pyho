33152f21753a3d4162a777dfe90e5f31a77b873a
jimmyho <ho@jimmyho.com>
2016-08-19 22:04:24 +0100

    yyyymmdd_to_dt

--- pyho/pyho.py
+++ pyho/pyho.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
 
 def laugh() -> str:
     return 'hohoho'
 
+
+def yyyymmdd_to_dt(yyyymmdd: int) -> datetime.date:
+    """
+    Convert date integer into date object.
+    eg, 20151225 -> date(2015, 12, 25)
+
+    :param yyyymmdd: date in integer
+    :return: datetime.date object
+    """
+
+    year = int(yyyymmdd / 10000)
+    month = int((yyyymmdd % 10000) / 100)
+    day = int(yyyymmdd % 100)
+
+    return datetime.date(year, month, day)
--- tests/test_pyho.py
+++ tests/test_pyho.py
@@ -8,16 +8,14 @@ test_pyho
 Tests for `pyho` module.
 """
 
-
 import sys
+import datetime
 import unittest
 
 from pyho import pyho
 
 
-
 class TestPyho(unittest.TestCase):
-
     def setUp(self):
         pass
 
@@ -27,6 +25,12 @@ class TestPyho(unittest.TestCase):
     def test_laugh(self):
         self.assertEqual(pyho.laugh(), 'hohoho')
 
+    def test_yyyymmdd_to_dt(self):
+        self.assertEqual(pyho.yyyymmdd_to_dt(20151225), datetime.date(2015, 12, 25))
+
+    def test_yyyymmdd_to_dt_error(self):
+        with self.assertRaises(ValueError):
+            pyho.yyyymmdd_to_dt(20161232)
 
 
 if __name__ == '__main__':
